bcb328ca112c1da62ce91df9d25bea57b88f6c14
Ashwini-Patil-02 <129053501+Ashwini-Patil-02@users.noreply.github.com>
2024-10-14 22:44:50 +0530

    Add files via upload

---
+++ factornum.java
@@ -0,0 +1,13 @@
+public class factornum {
+    public static void main(String[] args) {
+        
+    int num = 10;
+    System.out.println("factors of " + num+ " are ");
+
+    for(int i= 1; i<=num; i++)
+    {
+        if(num%i ==0)
+        System.out.println(i + " ");
+    }
+}
+}
---
+++ strong.java
@@ -0,0 +1,31 @@
+public class strong {
+    static int f[] = new int[10];
+    static void preComputer()
+    {
+        f[0] = f[1] = 1;
+        for(int i= 2; i<0; ++i)
+        f[i] = f[i-1]*i;
+    }
+    static boolean checkStrong(int num)
+    {
+        int sum = 0;
+        int temp = num;
+        while(temp>0)
+        {
+            sum += f[temp % 10];
+            temp = temp / 10;
+        }
+        return (sum == num);
+    }
+    public static void main(String[] args) {
+        preComputer();
+        int val = 145;
+
+        if(checkStrong(val))
+            System.out.println("Its a strong number");
+        else
+            System.out.println("Its not a strong number");
+    }
+}
+    
+
